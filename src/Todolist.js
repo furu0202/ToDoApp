72b743ac55d17ccfc66ec62da6f73b63f78a5570
furu0202 <furuka.0202@gmail.com>
2023-05-25 20:34:06 +0900

    タスクに関するボタンを追加

--- src/Todolist.js
+++ src/Todolist.js
@@ -4,6 +4,10 @@ const TodoList = () => {
   return (
     <div>
       <TodoList></TodoList>
+      <input type='text'></input>
+      <button>タスクの追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 };
