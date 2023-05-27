e16a52793fd19cff44a71c36816ed1174758f700
furu0202 <furuka.0202@gmail.com>
2023-05-27 17:25:09 +0900

    ボタンタグの記述ファイル変更およびuseStateの設定

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import TodoList from './Todolist';
 
 function App() {
+  const [todos, setTodos] = useState();
   return (
     <div>
       <TodoList></TodoList>
+      <input type='text'></input>
+      <button>タスクの追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
--- src/Todolist.js
+++ src/Todolist.js
@@ -4,10 +4,6 @@ const TodoList = () => {
   return (
     <div>
       <TodoList></TodoList>
-      <input type='text'></input>
-      <button>タスクの追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
     </div>
   );
 };
