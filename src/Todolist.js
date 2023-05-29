acda80649bb19aaed31aa93b3ddfac32b2bcb1a5
furu0202 <furuka.0202@gmail.com>
2023-05-29 23:59:19 +0900

    Todoコンポーネント作成

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const TodoList = (props) => {
-  return <div>{props.todos}</div>;
+const TodoList = ({ todos }) => {
+  return todos.map((todo) => <Todo todo={todo}></Todo>);
 };
 
 export default TodoList;
